3f6e17fc4144b785f462b4f8bd944e607acc0dda
amadzarac <121984306+amadzarac@users.noreply.github.com>
2023-09-08 12:27:15 -0700

    Add files via upload

--- public/server.js
+++ public/server.js
@@ -1,1 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const users = [];
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  const newUser = { email, password };
+  users.push(newUser);
+  res.redirect('/login');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+});
 
